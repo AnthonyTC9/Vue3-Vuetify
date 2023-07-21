3905518f53e58c9138e3f21dbeee923711a31764
Antonio Tapia C <tapia@Antonios-MacBook-Pro.local>
2023-07-20 20:32:41 -0700

    prueba final

---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-app-bar app color="black" dark flat class="px-12" >
+      <v-btn >
+        <v-icon color="yellow" left class="mr-2">fas fa-signature</v-icon> Tapiant
+      </v-btn>
+      
+  
+      <v-spacer></v-spacer>
+      <v-btn text @click="scroll('home')" class="text-yellow">Inicio</v-btn>
+      <v-btn text @click="scroll('about')"  >Nosotros</v-btn>
+      <v-btn text @click="scroll('portfolio')"  >Portafolio</v-btn>
+      <v-btn text @click="scroll('services')" > Servicios</v-btn>
+      <v-btn text @click="scroll('page')"  > Paginas</v-btn>
+      <v-btn text @click="scroll('biog')"  > Biog</v-btn>
+      <v-btn text @click="scroll('contact')"  > Contacto</v-btn>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      scroll(refName) {
+        const element = document.getElementById(refName);
+        element.scrollIntoView({ behavior: "smooth" });
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  </style>
